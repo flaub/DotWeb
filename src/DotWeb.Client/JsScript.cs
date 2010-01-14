b53391f7a3bb748a86cf9eab1162ad600c529209
Frank Laub <frank.laub@gmail.com>
2010-01-13 17:29:34 -0800

    Expose Window and Document thru JsScript again

---
+++ src/DotWeb.Client/JsScript.cs
@@ -0,0 +1,36 @@
+﻿// Copyright 2009, Frank Laub
+//
+// This file is part of DotWeb.
+//
+// DotWeb is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DotWeb is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with DotWeb.  If not, see <http://www.gnu.org/licenses/>.
+
+using DotWeb.Client.Dom;
+using DotWeb.Client.Dom.Html;
+using System.DotWeb;
+
+namespace DotWeb.Client
+{
+	public class JsScript
+	{
+		public extern Window Window {
+			[JsMacro("$wnd")]
+			get;
+		}
+
+		public extern HtmlDocument Document {
+			[JsMacro("$doc")]
+			get;
+		}
+	}
+}
