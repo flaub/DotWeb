29eb42907b965371c3ab48cd1a7e66a3b04b947a
Frank Laub <frank.laub@gmail.com>
2010-01-20 11:31:48 -0800

    Adding a test to show direct vs indirect method dependency on late-bound virtual calls. Beginning to add support in the method dependency analyzer

---
+++ test/DotWeb.Translator.Test/Expected/SystemTest/SystemTest_TestToStringDirect.js
@@ -0,0 +1,33 @@
+﻿$Namespace('System.Collections.Generic');
+
+System.Collections.Generic.List$1 = function() {
+};
+
+System.Collections.Generic.List$1.prototype.$ctor = function() {
+	this.items = new Array();
+	return this;
+};
+
+System.Collections.Generic.List$1.prototype.toString = function() {
+	var V_0 = "[ " + this.items.toString() + " ]";
+	return V_0;
+};
+
+$Namespace('System');
+
+System.Console = function() {
+};
+
+System.Console.WriteLine = function(value /*System.String*/) {
+	console.log(value);
+};
+
+$Namespace('H8');
+
+H8.SystemTests = function() {
+};
+
+H8.SystemTests.prototype.TestToStringDirect = function() {
+	var list = new System.Collections.Generic.List$1().$ctor();
+	System.Console.WriteLine(list.toString());
+};
---
+++ test/DotWeb.Translator.Test/Expected/SystemTest/SystemTest_TestToStringIndirect.js
@@ -0,0 +1,37 @@
+﻿$Namespace('System.Collections.Generic');
+
+System.Collections.Generic.List$1 = function() {
+};
+
+System.Collections.Generic.List$1.prototype.$ctor = function() {
+	this.items = new Array();
+	return this;
+};
+
+System.Collections.Generic.List$1.prototype.toString = function() {
+	var V_0 = "[ " + this.items.toString() + " ]";
+	return V_0;
+};
+
+$Namespace('System');
+
+System.Console = function() {
+};
+
+System.Console.WriteLine = function(value /*System.String*/) {
+	console.log(value);
+};
+
+$Namespace('H8');
+
+H8.SystemTests = function() {
+};
+
+H8.SystemTests.prototype.WriteObject = function(obj /*System.Object*/) {
+	System.Console.WriteLine(obj.toString());
+};
+
+H8.SystemTests.prototype.TestToStringIndirect = function() {
+	var list = new System.Collections.Generic.List$1().$ctor();
+	this.WriteObject(list);
+};
